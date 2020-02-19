e93b99a8bfda7bfcd3efec1a72467e451cd6903a
Joanna <i1johanna1s@gmail.com>
2020-02-19 12:02:19 +0200

    initial commit

---
+++ src/test/java/com/rickandmortyapi/RickandmortyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickandmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickandmortyapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
